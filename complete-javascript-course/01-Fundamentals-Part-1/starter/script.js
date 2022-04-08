635584dfee5eeb988a3a121081b0d140a72787f6
Adam Pham <thaihoangpham2008@gmail.com>
2022-04-09 00:01:42 +0800

    on learning

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -62,3 +62,18 @@
 // console.log(ageJonas, ageSarah, averageAge);
 
 
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Adam'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// let n ='1' + 1;
+// n = n -1;
+
+// console.log(n)
+
+ 
