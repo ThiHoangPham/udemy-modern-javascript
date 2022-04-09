9393c351deb5f25820479176eb8b298bf2611dd6
Adam Pham <thaihoangpham2008@gmail.com>
2022-04-09 19:03:06 +0800

    On Section 2

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -76,4 +76,29 @@
 
 // console.log(n)
 
- 
+// const age = 18;
+// if (age === 18) console.log("You just became an adult");
+
+// const hasDriverLicense = true; //A
+// const hasGoodVision = true; //B
+
+// console.log(hasDriverLicense && hasGoodVision);
+// console.log(hasDriverLicense || hasGoodVision);
+// console.log(!hasDriverLicense);
+
+// const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// // if(shouldDrive) {
+// //     console.log('Sarah is able to drive');
+// // }else{
+// //     console.log('Someone else should drive');
+// // }
+
+// const isTired = true;
+// console.log(hasDriverLicense && hasGoodVision && isTired);
+
+// if(shouldDrive) {
+//     console.log('Sarah is able to drive');
+// }else{
+//     console.log('Someone else should drive');
+// }
